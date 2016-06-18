453219f51a2a47bc4ebc971ca69eed4fd660b63a
Ygor Bruxel <ygor.bruxel@gmail.com>
2016-06-18 15:18:02 -0300

    Swap files that were wrong

--- lib/viacepex.ex
+++ lib/viacepex.ex
@@ -1,13 +1,5 @@
-defmodule Viacepex.Cep do
-  use HTTPoison.Base
-  defstruct [:cep, :logradouro, :complemento, :bairro, :localidade, :uf, :unidade, :ibge, :gia]
-
-  def process_url(cep) do
-    "https://viacep.com.br/ws/#{cep}/json/"
-  end
-
-  def process_response_body(body) do
-    body
-    |> Poison.decode!(keys: :atoms!)
+defmodule Viacepex do
+  def get!(cep) do
+    Viacepex.Cep.get!(cep).body
   end
 end
