c4f0ac4e486db12536cf22268f9cbb38cf33c602
Ygor <ygor.bruxel@gmail.com>
2017-03-18 16:07:19 -0300

    Add package info and prepare to publish

--- mix.exs
+++ mix.exs
@@ -5,12 +5,15 @@ defmodule Viacepex.Mixfile do
     [app: :viacepex,
      version: "0.0.2",
      elixir: "~> 1.2",
+     description: "A library to access ViaCEP api",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      deps: deps(),
      preferred_cli_env: [
         vcr: :test, "vcr.delete": :test, "vcr.check": :test, "vcr.show": :test
-     ] 
+     ],
+     source_url: "https://github.com/migore/viacepex"
     ]
   end
 
@@ -24,5 +27,11 @@ defmodule Viacepex.Mixfile do
       {:poison, "~> 3.0"},
       {:exvcr, "~> 0.8", only: :test}
     ]
+  end
+
+  defp package do
+    [maintainers: ["Ygor Bruxel"],
+     licenses: ["MIT"],
+     links: %{"Github" => "https://github.com/migore/viacepex"}]
   end
 end
